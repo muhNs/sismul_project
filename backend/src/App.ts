474969052d896900f2c65a66fb430cdba515c711
kevin8566 <kevinputrapratama856@gmail.com>
2026-06-11 08:25:31 +0700

    feat: add input password to add user

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -36,7 +36,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "authorization"],
   }),
 );
